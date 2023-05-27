025eca8358bbe7819474bb26f55143f324827a9c
Jaime Ferreira <jaimemcmf@Jaimes-MBP.home>
2023-05-27 23:07:58 +0100

    mais frases

--- src/change.pl
+++ src/change.pl
@@ -5,23 +5,37 @@ mae(rodrigo, helena).
 pai(vasco, jose).
 pai(rodrigo, francisco).
 
+
 suspect_name(_, carlos).
 
-respond([boa,tarde, N1|_], [chamo-me, N2]) :- change(N1, N2).
+respond([boa, tarde, carlos|_], [a, esta, hora, ja, e, boa, noite]).
+respond([boa, tarde, N1|_], [chamo-me, N2]) :- suspect_name(N1, N2).
+respond([bom, dia, carlos|_], [a, esta, hora, ja, e, boa, tarde]).
+respond([bom, dia, N1|_], [chamo-me, N2]) :- suspect_name(N1, N2).
+respond([ola], [agradecia, um, tratamento, mais, formal]).
+respond([ola, carlos|_], [agradecia, um, tratamento, mais, formal]).
+respond([ola, N1|_], [chamo-me, N2]) :- suspect_name(N1, N2).
 
 respond([foi, visto, na, N1|_],[nunca, estive, na, N1]).
 respond([foi, visto, no, N1|_],[nunca, estive, no, N1]).
 respond([foi, visto, em, N1|_],[nunca, estive, em, N1]).
-% opa e evitar (não sei se é possivel) ter tantos casos identicos como estes de cima
+
+respond([foi, visto, a, N1|_],[nunca, estive, em, N1]).
+
 respond([quando,viu,N1, N2, pela, ultima, vez], [nao, vejo, N1, N2, ha, meses]).
+
+respond([sabe, porque|_], [nao, sei, de, nada]).
+
+
 respond([conhece, o, N1|_], [N1, so, conheco, o, filho, da, N2]) :- mae(N1, N2).
 respond([conhece, o, N1|_], [N1, so, conheco, o, filho, do, N2]) :- pai(N1, N2).
 respond([conhece, o, N1|_], [nao, conheco, nenhum, N1]).
 respond([conhece, a, N1|_], [nao, conheco, nenhuma, N1]).
 
-respond([porque, estava|L1], [estava, ])
+respond([porque, estava|L1], L3) :- append([estava], L1, L2), append(L2, [como, costumo, todos, os, domingos], L3).
 
 respond([consegue, N1|_], [prefiro, N1, na, presenca, do, meu, advogado]).
+respond([pode, N1|_], [prefiro, N1, na, presenca, do, meu, advogado]).
 
 respond([confesse], [nunca, "!"]).
 
